5e71ca85b3c8e4a63b3601867478e48a9542236d
picpoint <picpoint@yandex.ru>
2020-11-02 21:34:44 +0300

    work with personal page

--- src/personalPage.php
+++ src/personalPage.php
@@ -11,6 +11,25 @@
   <section class="pp">
     <div class="pp__wrp">
       <div class="pp__topblock">
+        
+        <div class="pp__profile">
+          <div class="pp__logoblock">
+            <img src="pict/profileimg.png" alt="profile">
+          </div>
+          <div class="pp__nameblock">
+            <span>Иванов Иван</span>
+          </div>
+          <div class="pp__placeblock">
+            <span>г. Санкт-Петербург пер. Косыгина 23</span>
+          </div>
+          <div class="pp__detailblock">
+            <a href="#">Показать подробную информацию</a>
+          </div>
+        </div>
+
+        <div class="pp__topmenu">
+          
+        </div>
 
       </div>
 
