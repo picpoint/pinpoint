eea525a058af98714518c8481a127cef7439d49b
picpoint <picpoint@yandex.ru>
2020-11-11 19:47:34 +0300

    work with comments

--- controller/authorizationUserController.php
+++ controller/authorizationUserController.php
@@ -2,21 +2,21 @@
 
 
 
-class AuthorizationUserC {
+class AuthorizationUserC {                                              // класс авторизации пользователей
   
 
-  public function getDatasUserAuth() {
+  public function getDatasUserAuth() {                                  // метод получения данных от пользователя
     $arrAuthUser = [];
 
-    if(isset($_POST['authbth'])) {
-      if(!empty($_POST['authlogin']) && !empty($_POST['authpass'])) {
-        $arrAuthUser[] = trim($_POST['authlogin']);
+    if(isset($_POST['authbth'])) {                                      // если нажата кнопка "войти"
+      if(!empty($_POST['authlogin']) && !empty($_POST['authpass'])) {   // если поля логин и пароль не пусты
+        $arrAuthUser[] = trim($_POST['authlogin']);                     // в массив записываем логин и пароль
         $arrAuthUser[] = trim($_POST['authpass']);
         
-        $authUsr = new AuthorizationUserM();
-        $authUsr -> authUser($arrAuthUser);
+        $authUsr = new AuthorizationUserM();                            // вызываем модель авторизации
+        $authUsr -> authUser($arrAuthUser);                             // вызываем метод модели и передаём в неё массив с введёнными данными пользователя
       } else {
-        echo("Поля не заполнены");
+        echo("Поля не заполнены");                                      // иначе выдаём сообщение
       }
     }
     
--- controller/registrationUserController.php
+++ controller/registrationUserController.php
@@ -2,17 +2,17 @@
 
 
 
-class RegistrationUserC {
+class RegistrationUserC {                                                 // класс регистрации пользователей
   
-  public function getDatasUser() {
+  public function getDatasUser() {                                        // метод получения данных о пользователе
     $arrRegUser = [];
     
-    if(isset($_POST['regbtn'])) {
-      if(!empty($_POST['regfirstname']) && !empty($_POST['reglastname']) && !empty($_POST['reglogin']) && !empty($_POST['regpass'])) {
-        $arrRegUser[] = trim($_POST['regfirstname']);
-        $arrRegUser[] = trim($_POST['reglastname']);
-        $arrRegUser[] = trim($_POST['reglogin']);
-        $arrRegUser[] = trim(password_hash($_POST['regpass'], PASSWORD_DEFAULT));
+    if(isset($_POST['regbtn'])) {                                         // если нажата кнопка "регистрация"
+      if(!empty($_POST['regfirstname']) && !empty($_POST['reglastname']) && !empty($_POST['reglogin']) && !empty($_POST['regpass'])) {  // если поля не пустые
+        $arrRegUser[] = trim($_POST['regfirstname']);                     // в массив записываем имя
+        $arrRegUser[] = trim($_POST['reglastname']);                      // -||- записываем фамилию
+        $arrRegUser[] = trim($_POST['reglogin']);                         // -||- записываем логин
+        $arrRegUser[] = trim(password_hash($_POST['regpass'], PASSWORD_DEFAULT));  //-||- записываем хеш пароля 
 
         $regUser = new RegistrationUserM();
         $regUser -> regUserToDb($arrRegUser);
