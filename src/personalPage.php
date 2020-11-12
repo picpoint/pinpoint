578e1625897e68af36d7db464557d58ba33a3f04
picpoint <picpoint@yandex.ru>
2020-11-12 09:05:29 +0300

    work with sendform

--- src/personalPage.php
+++ src/personalPage.php
@@ -18,93 +18,107 @@ session_start();
   
   <section class="pp">
     <div class="pp__wrp" id="map">
-
-        <div class="pp__profile">
-          <div class="pp__logoblock">
-            <img src="pict/profileimg.png" alt="profile">
+      
+      <div class="pp__sendfile">
+        <div class="pp__hdrsf">
+          <div class="pp__hdricon">
+            <img src="pict/pinWhite.png" alt="iconpin">
           </div>
-          <div class="pp__nameblock">
-            <span>
-              <?php
-                echo($_SESSION['login']);
-              ?>
-            </span>
+          <div class="pp__hdrplace">
+            <span>Россия, Москва, ул. Ленина 52</span>
           </div>
-          <div class="pp__placeblock">
-            <span>г. Санкт-Петербург пер. Косыгина 23</span>
-          </div>
-          <div class="pp__detailblock">
-            <a href="#">Показать подробную информацию</a>
+          <div class="pp__hdrclose">
+            <span>+</span>
           </div>
         </div>
+      </div>
 
-        <div class="pp__topmenu">
-          <div class="pp__basemenublock">
-            <div class="pp__messageblock">
-              <a href="#">
-                <i class="fa fa-envelope-o" aria-hidden="true"></i>
-                <span>сообщения</span>
-              </a>              
-            </div>
+      <div class="pp__profile">
+        <div class="pp__logoblock">
+          <img src="pict/profileimg.png" alt="profile">
+        </div>
+        <div class="pp__nameblock">
+          <span>
+            <?php
+              echo($_SESSION['login']);
+            ?>
+          </span>
+        </div>
+        <div class="pp__placeblock">
+          <span>г. Санкт-Петербург пер. Косыгина 23</span>
+        </div>
+        <div class="pp__detailblock">
+          <a href="#">Показать подробную информацию</a>
+        </div>
+      </div>
 
-            <div class="pp__pinsblock">
-              <a href="#">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span>метки</span>
-              </a>              
-            </div>
-
-            <div class="pp__freindsblock">
-              <a href="#">
-                <i class="fa fa-users" aria-hidden="true"></i>
-                <span>друзья</span>
-              </a>              
-            </div>
-
-            <div class="pp__newsblock">
-              <a href="#">
-                <i class="fa fa-bullhorn" aria-hidden="true"></i>
-                <span>новости</span>
-              </a>              
-            </div>
-            
-            <div class="pp__waysblock">
-              <a href="#">
-                <i class="fa fa-globe" aria-hidden="true"></i>
-                <span>маршруты</span>
-              </a>              
-            </div>
-
-            <div class="pp__albumsblock">
-              <a href="#">
-                <i class="fa fa-picture-o" aria-hidden="true"></i>
-                <span>альбомы</span>
-              </a>              
-            </div>
-
-            <div class="pp__findblock">
-              <a href="#">
-                <i class="fa fa-search" aria-hidden="true"></i>
-                <span>поиск</span>
-              </a>              
-            </div>
-
-            <div class="pp__settingsblock">
-              <a href="#">
-                <i class="fa fa-cogs" aria-hidden="true"></i>
-                <span>настройка</span>
-              </a>              
-            </div>
-          </div>
-
-          <div class="pp__exitblock">
-            <a href="index.php">
-              <i class="fa fa-sign-out" aria-hidden="true"></i>
-              <span>выход</span>
+      <div class="pp__topmenu">
+        <div class="pp__basemenublock">
+          <div class="pp__messageblock">
+            <a href="#">
+              <i class="fa fa-envelope-o" aria-hidden="true"></i>
+              <span>сообщения</span>
             </a>              
           </div>
 
+          <div class="pp__pinsblock">
+            <a href="#">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>метки</span>
+            </a>              
+          </div>
+
+          <div class="pp__freindsblock">
+            <a href="#">
+              <i class="fa fa-users" aria-hidden="true"></i>
+              <span>друзья</span>
+            </a>              
+          </div>
+
+          <div class="pp__newsblock">
+            <a href="#">
+              <i class="fa fa-bullhorn" aria-hidden="true"></i>
+              <span>новости</span>
+            </a>              
+          </div>
+          
+          <div class="pp__waysblock">
+            <a href="#">
+              <i class="fa fa-globe" aria-hidden="true"></i>
+              <span>маршруты</span>
+            </a>              
+          </div>
+
+          <div class="pp__albumsblock">
+            <a href="#">
+              <i class="fa fa-picture-o" aria-hidden="true"></i>
+              <span>альбомы</span>
+            </a>              
+          </div>
+
+          <div class="pp__findblock">
+            <a href="#">
+              <i class="fa fa-search" aria-hidden="true"></i>
+              <span>поиск</span>
+            </a>              
+          </div>
+
+          <div class="pp__settingsblock">
+            <a href="#">
+              <i class="fa fa-cogs" aria-hidden="true"></i>
+              <span>настройка</span>
+            </a>              
+          </div>
         </div>
+
+        <div class="pp__exitblock">
+          <a href="index.php">
+            <i class="fa fa-sign-out" aria-hidden="true"></i>
+            <span>выход</span>
+          </a>              
+        </div>
+
+      </div>
 
       
       <div class="pp__bottomblock">
