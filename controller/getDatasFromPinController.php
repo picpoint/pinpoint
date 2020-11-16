f836063b0b3b6ab10b5ebfe38de53ad5399ea26f
picpoint <picpoint@yandex.ru>
2020-11-16 08:52:22 +0300

    work with upload file

--- controller/getDatasFromPinController.php
+++ controller/getDatasFromPinController.php
@@ -3,6 +3,7 @@
 
 
 class GetDatasFromPinC {
+  public $pathToUsersFolder = __DIR__ . '/../usersFolders/';
   
 
   public function addPins() {
@@ -13,13 +14,29 @@ class GetDatasFromPinC {
 
       if($_FILES['upfile']['size'] != 0) {
         var_dump($_FILES['upfile']);
+        echo($this->pathToUsersFolder);
+        echo("<br>");
+        $pathToFolder = $this->pathToUsersFolder . $_SESSION['login'];        
+        echo($pathToFolder);
+
+        if(!file_exists($pathToFolder)) {
+          mkdir($pathToFolder, 0777);
+          mkdir($pathToFolder . '/pict', 0777);          
+        } else {
+
+        }
+
+
       }
 
       
       
+
       if($_POST['comments'] != 0) {
+        echo("<br>");
         var_dump($_POST['comments']);
       } else {
+        echo("<br>");
         var_dump($_POST['comments']);
       }
 
