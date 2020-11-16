e75d6d1c06c2dc1aa22405cd37fa873f2b016c9b
picpoint <picpoint@yandex.ru>
2020-11-16 11:38:49 +0300

    work with add pin

--- controller/getDatasFromPinController.php
+++ controller/getDatasFromPinController.php
@@ -7,29 +7,27 @@ class GetDatasFromPinC {
   
 
   public function addPins() {
-    if(isset($_POST['addpinbtn'])) {
-      // var_dump($_FILES['upfile']);
-      // echo("<br>");
-      // var_dump($_POST['comments']);
+    if(isset($_POST['addpinbtn'])) {      
 
       if($_FILES['upfile']['size'] != 0) {
-        var_dump($_FILES['upfile']);
-        echo($this->pathToUsersFolder);
+        // var_dump($_FILES['upfile']);        
         echo("<br>");
-        $pathToFolder = $this->pathToUsersFolder . $_SESSION['login'];        
-        echo($pathToFolder);
+        $pathToFolder = $this->pathToUsersFolder . $_SESSION['login'];                
 
         if(!file_exists($pathToFolder)) {
           mkdir($pathToFolder, 0777);
-          mkdir($pathToFolder . '/pict', 0777);          
+          mkdir($pathToFolder.'/pict', 0777);
+          mkdir($pathToFolder.'/video', 0777);          
+          if($_FILES['upfile']['type'] == 'image/jpeg' || $_FILES['upfile']['type'] == 'image/jpg' || $_FILES['upfile']['type'] == 'image/png') {
+            move_uploaded_file($_FILES['upfile']['tmp_name'], $pathToFolder.'/pict/'. $_FILES['upfile']['name']);
+          }
         } else {
-
+          if($_FILES['upfile']['type'] == 'image/jpeg' || $_FILES['upfile']['type'] == 'image/jpg' || $_FILES['upfile']['type'] == 'image/png') {
+            move_uploaded_file($_FILES['upfile']['tmp_name'], $pathToFolder.'/pict/'. $_FILES['upfile']['name']);
+          }
         }
 
-
       }
-
-      
       
 
       if($_POST['comments'] != 0) {
