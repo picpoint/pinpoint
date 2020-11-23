71d89c83f1c5fad4d6896136a8d57d64008344ab
picpoint <picpoint@yandex.ru>
2020-11-23 21:43:30 +0300

    work with show pins

--- controller/getDataPinContentController.php
+++ controller/getDataPinContentController.php
@@ -13,13 +13,18 @@ class GetDataPinContentC {
     $res = $dataPins -> getDataPinContent($login);    
     
     foreach($res as $rs) {
-      foreach($rs as $key => $value) {        
-        $arrDt[] = $value;
+      foreach($rs as $key => $value) {
+        // echo("$key - $value");
+        // echo("<br>");
+        $arrDt[] = "$key - $value";
       }
     }
+    
+    
+    return $arrDt;
 
     // return json_encode($arrDt, JSON_UNESCAPED_UNICODE);
-    return $arrDt;
+    // return $res;
 
   }
   
--- src/js/showBallons.js
+++ src/js/showBallons.js
@@ -1,9 +1,10 @@
 // console.log(arrdt);
-// let arrPins = arrdt.getAttribute("data-arrpins");
-// console.log(arrPins);
+let arrPins = arrdt.getAttribute("data-arrpins");
+console.log(arrPins);
+
 
 let rawStr = arrPins.slice(19, arrPins.length - 6);
-console.log(rawStr);
+// console.log(rawStr);
 
 
 
