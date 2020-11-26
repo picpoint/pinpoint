f08e557bc3c90b4005c7d1b39d5499f2fb0a91e8
picpoint <picpoint@yandex.ru>
2020-11-26 20:03:13 +0300

    work with show pins

--- src/js/showBallons.js
+++ src/js/showBallons.js
@@ -18,11 +18,15 @@ for(let i = 0; i < arr1.length; i++) {                             // переб
   
 }
 
-console.log(alldts);
+// console.log(alldts);
 
 
-for(let x = 0; x < alldts.length; x++) {
+for(let x = 0, y = 0; x < alldts.length; x++) {
+  // console.log(alldts[x]);
+  y++;
+
   let onms = alldts[x].split(' - ');
+  console.log(onms);
 
   if(onms[0] == 'photo') {
     photoPin = onms[1];
@@ -34,28 +38,73 @@ for(let x = 0; x < alldts.length; x++) {
     longitudePin = onms[1];
   }
 
+
   
 
 
-  placemark = new ymaps.Placemark([latitudePin, longitudePin], {      
-    balloonContentHeader: '<a href = "#">ОБЖОРА</a><br>' +
-      '<span class="description"> Пицерия </span>',      
-    balloonContentBody: '<img src=" ' + photoPin + ' " height="150" width="200"> <br/> ' +
-      commentsPin,
-    hintContent: commentsPin
-  });
-  
-  /*
-    balloonContentBody: '<img src="../usersFolders/freeAlex/pict/18b0a4b31ad55084690a46198f88a671_XL.jpg" height="150" width="200"> <br/> ' +
-  */
-  
-  // Добавим метку на карту.
-  myMap.geoObjects
-    .add(placemark);
-    // Откроем балун на метке.
-    // placemark.balloon.open();
+  if(y == 4) {
+
+    placemark = new ymaps.Placemark([latitudePin, longitudePin], {      
+      balloonContentHeader: '<a href = "#">ОБЖОРА</a><br>' +
+        '<span class="description"> Пицерия </span>',      
+      balloonContentBody: '<img src=" ' + photoPin + ' " height="150" width="200"> <br/> ' +
+        commentsPin,
+      hintContent: commentsPin
+    });
+    
+
+    myMap.geoObjects
+      .add(placemark);
+
+      console.log('-------');
+      y = 0;
+    }
 
 
 }
 
-// console.log('/../' + photoPin);
+
+
+
+
+
+
+
+// for(let x = 0; x < alldts.length; x++) {
+//   let onms = alldts[x].split(' - ');
+
+//   if(onms[0] == 'photo') {
+//     photoPin = onms[1];
+//   } else if(onms[0] == 'commentaries') {
+//     commentsPin = onms[1];
+//   } else if(onms[0] == 'latitude') {
+//     latitudePin = onms[1];
+//   } else if(onms[0] == 'longitude') {
+//     longitudePin = onms[1];
+//   }
+
+  
+
+
+//   placemark = new ymaps.Placemark([latitudePin, longitudePin], {      
+//     balloonContentHeader: '<a href = "#">ОБЖОРА</a><br>' +
+//       '<span class="description"> Пицерия </span>',      
+//     balloonContentBody: '<img src=" ' + photoPin + ' " height="150" width="200"> <br/> ' +
+//       commentsPin,
+//     hintContent: commentsPin
+//   });
+  
+//   /*
+//     balloonContentBody: '<img src="../usersFolders/freeAlex/pict/18b0a4b31ad55084690a46198f88a671_XL.jpg" height="150" width="200"> <br/> ' +
+//   */
+  
+//   // Добавим метку на карту.
+//   myMap.geoObjects
+//     .add(placemark);
+//     // Откроем балун на метке.
+//     // placemark.balloon.open();
+
+
+// }
+
+// // console.log('/../' + photoPin);
