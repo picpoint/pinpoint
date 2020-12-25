5974b8e92889a67fe180f7809449d0db6cd151d3
picpoint <picpoint@yandex.ru>
2020-12-25 09:05:06 +0300

    work with write messages to freinds

--- src/js/selectFreindToMessages.js
+++ src/js/selectFreindToMessages.js
@@ -1,2 +1,16 @@
 const msgsblock = document.querySelector('.msg__allusrs');
-console.log(msgsblock);
+let msgsblockChildrens = msgsblock.children;
+
+// console.log(msgsblock);
+// console.log(msgsblockChildrens);
+
+for(let x = 0; x < msgsblockChildrens.length; x++) {
+  console.log(msgsblockChildrens[x]);
+  // console.log(msgsblockChildrens[x].lastElementChild.value);
+
+  msgsblockChildrens[x].addEventListener('click', () => {
+    console.log(msgsblockChildrens[x].lastElementChild.value);
+  });
+
+
+}
