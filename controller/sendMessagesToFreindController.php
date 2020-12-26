7c49cd8057618f401c7045bba0050ccf92a7a951
picpoint <picpoint@yandex.ru>
2020-12-26 23:38:46 +0300

    add comments

--- controller/sendMessagesToFreindController.php
+++ controller/sendMessagesToFreindController.php
@@ -2,18 +2,18 @@
 
 
 
-class SendMessageToOneFreindC {
+class SendMessageToOneFreindC {                                           // класс контроллер отправки сообщения пользователю
   
-	public function sendMessage() {
-    $login = $_SESSION['login'];    
+	public function sendMessage() {                                         // метод отправки
+    $login = $_SESSION['login'];                                          // в $login записываем логин текущего пользователя из сессии
     
-    if(isset($_POST['sendmsgbtn'])) {
-      $idmsgtofrnd = $_POST['msgtofrnd'];
-      $msg = $_POST['sendmsgfield'];
+    if(isset($_POST['sendmsgbtn']) && !empty($_POST['sendmsgfield'])) {   // если нажата кнопка "отправить" и не пустое поле с текстом
+      $idmsgtofrnd = $_POST['msgtofrnd'];                                 // получаем id скрытого поля input той записи где дружба с другом
+      $msg = $_POST['sendmsgfield'];                                      // получаем само сообщение
 
-      $getData = new WriteMessagesToDB();
-      $getData -> selectUsersForMessages($idmsgtofrnd);
-      $getData -> writeMessage($msg);
+      $getData = new WriteMessagesToDB();                                 // вызываем модель для записи сообщения в бд
+      $getData -> selectUsersForMessages($idmsgtofrnd);                   // вызов метода выбора юзера для сообщения
+      $getData -> writeMessage($msg);                                     // метод вызова записи сообщения в бд
 		}
 
 	}
--- model/writeToDBmessagesModel.php
+++ model/writeToDBmessagesModel.php
@@ -2,35 +2,35 @@
 
 
 
-class WriteMessagesToDB {
-  public $resSelect;
+class WriteMessagesToDB {                                                         // класс модель записи сообщения в бд
+  public $resSelect;                                                              // публичная переменная для передачи данных из метода в метод
 
-	public function __construct() {
+	public function __construct() {                                                 // конструктор соединения с бд
 		$this->cnnct = new PDO('mysql:host=localhost;dbname=ppdb', 'rmtar', '2203');
 	}
 
 
   
-  public function selectUsersForMessages($idmsgtofrnd) {
-    $sth = $this->cnnct -> prepare("SELECT id_user, id_myfreind FROM freinds WHERE id_freinds = '$idmsgtofrnd'");
-    $sth -> execute();
-    $this->resSelect = $sth -> fetchAll(PDO::FETCH_ASSOC);
-    return $this->resSelect;
+  public function selectUsersForMessages($idmsgtofrnd) {                          // метод получения данных по конкретному пользователю
+    $sth = $this->cnnct -> prepare("SELECT id_user, id_myfreind FROM freinds WHERE id_freinds = '$idmsgtofrnd'"); // выбор конкретной записи из бд, где пользователю друзья
+    $sth -> execute();                                                            // выполняем запрос
+    $this->resSelect = $sth -> fetchAll(PDO::FETCH_ASSOC);                        // получаем ассоциативный массив
+    return $this->resSelect;                                                      // возвращяем результат
   }
 
 
 
-  public function writeMessage($msg) {
-    $msgFromUser;
-    $msgToUser;
-    $writeMsg = $this->resSelect;
+  public function writeMessage($msg) {                                            // метод записи сообщения в бд
+    $msgFromUser;                                                                 // переменная для определения от какого юзера будет сообщение
+    $msgToUser;                                                                   // переменная для определения кому будет сообщение
+    $writeMsg = $this->resSelect;                                                 // массив с данными по пользователям
 
-    foreach($writeMsg as $arrms) {
-      $msgFromUser = $arrms['id_myfreind'];
-      $msgToUser = $arrms['id_user'];
+    foreach($writeMsg as $arrms) {                                                // перебираем массив с данными
+      $msgFromUser = $arrms['id_myfreind'];                                       // записываем в $msgFromUser от кого будет сообщение
+      $msgToUser = $arrms['id_user'];                                             // записываем в $msgToUser кому будет сообщение
 
-      $sth = $this->cnnct -> prepare("INSERT INTO messages (id_frommsg, id_tomsg, messag) VALUES('$msgFromUser', '$msgToUser', '$msg')");
-      $sth -> execute();
+      $sth = $this->cnnct -> prepare("INSERT INTO messages (id_frommsg, id_tomsg, messag) VALUES('$msgFromUser', '$msgToUser', '$msg')"); // вставляем в бд данные от кого и кому будет сообщение и само сообщение
+      $sth -> execute();                                                          // выполняем запрос
     }
 
   }
