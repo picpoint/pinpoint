dc258c9ec16fd2b4096261070053b44adf33e1d1
picpoint <picpoint@yandex.ru>
2020-12-30 16:31:24 +0300

    work with chat

--- src/js/selectFreindToMessages.js
+++ src/js/selectFreindToMessages.js
@@ -4,8 +4,9 @@ const msgtofrnd = document.querySelector('.msg__msgtofrnd');
 
 
 
-for(let x = 0; x < msgsblockChildrens.length; x++) {
-  msgsblockChildrens[x].addEventListener('click', () => {    
+for(let x = 0; x < msgsblockChildrens.length; x++) {  
+  msgsblockChildrens[x].addEventListener('click', () => {
+    // msgsblockChildrens[x].style = 'box-shadow: 0 0 10px rgba(0,0,0,0.5)';    
     msgtofrnd.value = msgsblockChildrens[x].lastElementChild.value;
     
   });
