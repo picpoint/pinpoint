e64f760bc2e88c08bf455db7aa127b9df86e20c5
picpoint <picpoint@yandex.ru>
2021-01-09 21:48:30 +0300

    work with chat

--- controller/readFreindsMessagesController.php
+++ controller/readFreindsMessagesController.php
@@ -11,15 +11,12 @@ class ReadFreindsMessagesC {
 
     foreach($frndWithMsg as $fwm) {                                    // перебираем массив для вывода блока с другом
       ?>
-        <!-- <form action="get" name="readmsg"> -->
-          <div class="msg__dialog">           
-            <img src="pict/avatarka.png" alt="avatar">            
-            <span><?php echo($fwm['firstname']);?></span>
-            <span><?php echo($fwm['lastname']);?></span>
-            <input type="text" name="hideid" value="<?php echo($fwm['id_freinds']);?>">
-          </div>
-        <!-- </form> -->
-
+        <div class="msg__dialog">           
+          <img src="pict/avatarka.png" alt="avatar">            
+          <span><?php echo($fwm['firstname']);?></span>
+          <span><?php echo($fwm['lastname']);?></span>
+          <input type="text" name="hideid" value="<?php echo($fwm['id_freinds']);?>">
+        </div>
       <?php
     }   
 
--- model/readFreindsMessagesModel.php
+++ model/readFreindsMessagesModel.php
@@ -13,13 +13,12 @@ class ReadFreindsMessagesM {
     $sth = $this->cnnct -> prepare("SELECT DISTINCT users.firstname, users.lastname, freinds.id_freinds
                                     FROM $login 
                                     JOIN users 
-                                      ON $login.id_frommsg != '$login' AND den.id_frommsg = users.login 
+                                      ON $login.id_frommsg != '$login' AND $login.id_frommsg = users.login 
                                     JOIN freinds 
                                       ON freinds.id_user = $login.id_frommsg");
     $sth -> execute();
     $data = $sth -> fetchAll(PDO::FETCH_ASSOC);
     return $data;
-
   }
 
 
