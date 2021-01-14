cd9319b75f038244a135a622207521807d75217a
picpoint <picpoint@yandex.ru>
2021-01-14 09:01:48 +0300

    work with chat

--- dist/js/ajaxReadFreindsMessages.js
+++ dist/js/ajaxReadFreindsMessages.js
@@ -3,17 +3,20 @@ const correspondence = document.querySelector('.msg__correspondence');
 const currentLogin = document.querySelector('.msg__curlog');
 
 
+for(let x = 0; x < correspondence.children.length; x++) {
+  correspondence.children[x].remove();
+}
 
 
 for(let x = 0; x < allUsers.children.length; x++) {
   allUsers.children[x].addEventListener('click', (e) => {    
     let idFreinds = allUsers.children[x].lastElementChild.value;
-    
+
     const req = new XMLHttpRequest();
     const url = "http://localhost/pinpoint/dist/messages.php?idfrnd=" + idFreinds;
     req.open("GET", url, true);
-    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");    
+
     req.addEventListener("readystatechange", () => {
       if(req.readyState == 4 && req.status == 200) {
         strResData = req.response;
@@ -59,12 +62,3 @@ for(let x = 0; x < allUsers.children.length; x++) {
   });
 
 }
-
-
-
-// console.log(correspondence.children);
-
-for(let x = 0; x < correspondence.children.length; x++) {
-  console.log(correspondence.children[x]);
-}
-
