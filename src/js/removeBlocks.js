aee3a855ed2fb95458e82ae44f13385bfcba3b3a
picpoint <picpoint@yandex.ru>
2021-01-15 08:53:39 +0300

    work with chat

--- src/js/removeBlocks.js
+++ src/js/removeBlocks.js
@@ -5,13 +5,10 @@ const correspondence = document.querySelector('.msg__correspondence');
 
 for(let z = 0; z < allUsers.children.length; z++) {
   // console.log(allUsers.children[z]);
-
   allUsers.children[z].addEventListener('click', () => {
-    console.log(correspondence.children);
-
+    // console.log(correspondence.children);
     for(let y = 0; y < correspondence.children.length; y++) {
-      console.log(correspondence.children[y].remove());
+      console.log(correspondence.children[y]);
     }
-
   });
 }
