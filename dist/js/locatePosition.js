299aae13132f2449afc20ccb082b34e53a0268ed
picpoint <picpoint@yandex.ru>
2021-02-04 23:29:14 +0300

    work with location on map

--- dist/js/locatePosition.js
+++ dist/js/locatePosition.js
@@ -3,27 +3,20 @@ let position;
 window.addEventListener("load", (e) => {
   let arrCurrentCoords = [];  
   
-  if (e.type == 'load') {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setTimeout(() => {
-        if(typeof(position) == "object") {
-          let lat = (position.coords.latitude).toFixed(6);
-          let lng = (position.coords.longitude).toFixed(6);              
-          arrCurrentCoords.push(+lat);
-          arrCurrentCoords.push(+lng);
-          console.log(arrCurrentCoords);
-        } else if(isEmpty(position)){
-          console.log("Позиция не определена");
-        }
-      }, 3000);
-      
-
+  if (e.type == 'load') {    
+    navigator.geolocation.getCurrentPosition((position) => {      
+      let lat = (position.coords.latitude).toFixed(6);
+      let lng = (position.coords.longitude).toFixed(6);              
+      arrCurrentCoords.push(+lat);
+      arrCurrentCoords.push(+lng);
+      console.log(arrCurrentCoords);
+    }, error => {
+      console.error("Местоположение не определено ...");
     });
-
-    
-
   } else {    
     console.log('Страница по определённым проблемам не загрузилась до конца ... ');
   }
+
+  
 
 });
