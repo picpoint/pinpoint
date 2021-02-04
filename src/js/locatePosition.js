9a529d4217f99e0861ccef892677db73980fb766
picpoint <picpoint@yandex.ru>
2021-02-04 16:49:45 +0300

    work with position

--- src/js/locatePosition.js
+++ src/js/locatePosition.js
@@ -1,15 +1,31 @@
+
+let position;
 window.addEventListener("load", (e) => {
-  let arrCurrentCoords = [];
+  let arrCurrentCoords = [];  
+  
   if (e.type == 'load') {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = (position.coords.latitude).toFixed(6);
       let lng = (position.coords.longitude).toFixed(6);    
       console.log("my lat - " + lat);      
       console.log("my long - " + lng);
-      arrCurrentCoords.push(lat);
-      arrCurrentCoords.push(lng);
-      console.log(arrCurrentCoords);
+      arrCurrentCoords.push(+lat);
+      arrCurrentCoords.push(+lng);
+      console.log(arrCurrentCoords);      
+
+      setTimeout(() => {
+        if(typeof(position) == "object" && position) {
+          console.log("yes");
+        } else if(isEmpty(position)){
+          console.log("Позиция не определена");
+        }
+      }, 3000);
+      
+
     });
+
+    
+
   } else {    
     console.log('Страница по определённым проблемам не загрузилась до конца ... ');
   }
