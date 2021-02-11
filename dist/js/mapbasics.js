9dd554d85ca4a678bf539c67c5097f66225d6fb8
picpoint <picpoint@yandex.ru>
2021-02-11 08:57:15 +0300

    work with coords

--- dist/js/mapbasics.js
+++ dist/js/mapbasics.js
@@ -4,48 +4,14 @@ const arrdt = document.querySelector('.pp__arrdt');
 var myMap;                                                                                // инициализация переменной для использования в ф-ии init
 
 
-
-
-function locatePosition() {
-  let arrCurrentCoords = [];
-  return new Promise((resolve, reject) => {
-    window.addEventListener("load", (e) => {
-      if (e.type == 'load') {
-        navigator.geolocation.getCurrentPosition((position) => {      
-          let lat = (position.coords.latitude).toFixed(6);
-          let lng = (position.coords.longitude).toFixed(6);              
-          arrCurrentCoords.push(+lat);
-          arrCurrentCoords.push(+lng);
-          // console.log(arrCurrentCoords);
-          return arrCurrentCoords;
-          // window.curCoords = arrCurrentCoords;
-        }, error => {
-          console.error("Местоположение не определено ...");
-        });
-      } else {    
-        console.log('Страница по определённым проблемам не загрузилась до конца ... ');
-      }
-    });
-    
-    resolve(arrCurrentCoords);
-  });
-
-}
-
-
-locatePosition()
-  .then((result) => {
-    // ymaps.ready(init(result));
-    console.log(result);
-  });
+let res = new DetermineLocatePosition();
+let coordinates = res.locatePosition();
 
 
 
+ymaps.ready(init);                                                                        // инициализируется карта
 
 
-
-
-// ymaps.ready(init);                                                                        // инициализируется карта
 
 
 
@@ -58,7 +24,7 @@ function includeShowBalloons(url) {                                             
 
 
 
-function init (coordinates) {                                                                      // ф-ия инициализации
+function init () {                                                                      // ф-ия инициализации
   myMap = new ymaps.Map("map", {
     center: coordinates || [55.7538, 37.6201],
     // center: [55.7538, 37.6201],                                                      // координаты места загрузки и показа карты
