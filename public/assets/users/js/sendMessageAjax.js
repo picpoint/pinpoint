1f3f4221ee671a03af1b0a70d233e82ace49cee3
rmtar <picpoint@yandex.ru>
2022-02-24 22:23:28 +0300

    made page about

--- public/assets/users/js/sendMessageAjax.js
+++ public/assets/users/js/sendMessageAjax.js
@@ -7,36 +7,38 @@ let token = sendmsgform.firstElementChild;
 sendmsgform.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // const request = new XMLHttpRequest();
-    // const url = "/messages";
-    // // const params = "_token=" + token.value + "&idfreind=" + idfreind.value + "&fieldmessage=" + fieldmessage.value;
-    // // const params = "_token=" + token.value + '&currentuser_id=123&user_id=123&message=123;
-    //
-    //
-    // const params = [{
-    //     "_token": token.value,
-    //     'currentuser_id': '123',
-    //     'user_id': '123',
-    //     'message': '123'
-    // }];
-    //
-    //
-    //
-    // console.log(params);
-    //
-    //
-    // request.open("POST", url, true);
-    // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    //
-    // request.addEventListener("readystatechange", function() {
-    //
-    //     if(request.readyState === 4 && request.status === 200) {
-    //         console.log(request.responseText);
-    //     }
-    //
-    // });
-    //
-    // request.send(params);
+    console.log('click btn');
+
+    const request = new XMLHttpRequest();
+    const url = "/messages";
+    // const params = "_token=" + token.value + "&idfreind=" + idfreind.value + "&fieldmessage=" + fieldmessage.value;
+    // const params = "_token=" + token.value + '&currentuser_id=123&user_id=123&message=123;
+
+
+    const params = [{
+        "_token": token.value,
+        'currentuser_id': '123',
+        'user_id': '123',
+        'message': '123'
+    }];
+
+
+
+    console.log(params);
+
+
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.addEventListener("readystatechange", function() {
+
+        if(request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+        }
+
+    });
+
+    request.send(params);
 
 });
 
