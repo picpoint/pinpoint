9dd4956c0d245afb2b8eef8a058064ddb4b39a51
rmtar <picpoint@yandex.ru>
2022-04-02 19:13:07 +0300

    work with pahe settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SettingsController extends Controller
 {
 
     public function index() {
         $title = 'Настройки | Pinpoint';
-        return view('user.settings', compact('title'));
+        $id = Auth::user()->id;
+        $raw = DB::table('users')->where('id', '=', $id)->get();
+        $user = $raw[0];
+
+        $dateOfReg = Carbon::parse($user->created_at)->format('d.m.Y');
+
+        return view('user.settings', compact('title', 'user', 'dateOfReg'));
     }
 
 }
--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -20,7 +20,56 @@
 
         <div class="settings__content">
             <div class="settings__contentwrp">
-                
+                <div class="settings__cardofuser">
+                    <div class="settings__leftcard">
+                        <div class="settings__lftavatar">
+                            <a href="#">
+                                <img src="/public/assets/users/img/avatar.png" alt="avatar">
+                            </a>
+                        </div>
+                        <div class="settings__lftsurname">
+                            <span>Фамилия</span>
+                        </div>
+                        <div class="settings__lftname">
+                            <span>Имя</span>
+                        </div>
+                        <div class="settings__lftpatronymic">
+                            <span>Отчество</span>
+                        </div>
+                        <div class="settings__lftemail">
+                            <span>email</span>
+                        </div>
+                        <div class="settings__lftphone">
+                            <span>телефон</span>
+                        </div>
+                        <div class="settings__lftdateofreg">
+                            <span>дата регистрации</span>
+                        </div>
+                    </div>
+                    <div class="settings__rightcard">
+                        <div class="settings__rghtavatar">
+
+                        </div>
+                        <div class="settings__rghtsurname">
+                            <a href="#">Редактировать</a>
+                        </div>
+                        <div class="settings__rghtname">
+                            <a href="#">{{ $user->name }}</a>
+                        </div>
+                        <div class="settings__rghtpatronymic">
+                            <a href="#">Редактировать</a>
+                        </div>
+                        <div class="settings__rghtemail">
+                            <a href="#">{{ $user->email }}</a>
+                        </div>
+                        <div class="settings__rghtphone">
+                            <a href="#">Редактировать</a>
+                        </div>
+                        <div class="settings__rghtdateofreg">
+                            <a>{{ $dateOfReg }}</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
