ddd0f48fe3bf05900064a7f044b68eb301bcd9fd
picpoint <picpoint@yandex.ru>
2022-04-18 13:23:35 +0300

    work with design

--- resources/views/user/messagesto.blade.php
+++ resources/views/user/messagesto.blade.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="/public/assets/users/css/font-awesome.min.css">
+    {{--<link rel="stylesheet" href="/public/assets/users/css/font-awesome.min.css">--}}
+    <link rel="stylesheet" href="/public/assets/users/css/all.css">
     <link rel="stylesheet" href="/public/assets/users/css/style.css">
     <title>{{ $title }}</title>
 </head>
