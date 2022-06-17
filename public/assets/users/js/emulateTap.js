4767ff3b629be9290fb27a38b5022ab5cade3e27
picpoint <picpoint@yandex.ru>
2022-06-17 12:26:29 +0300

    work with emulate tap

--- public/assets/users/js/emulateTap.js
+++ public/assets/users/js/emulateTap.js
@@ -1,1 +1,20 @@
-console.log("Emulate js");
+var myMap = document.querySelector('#map');
+
+console.log(myMap);
+
+
+var hammertime = new Hammer(myMap, {
+    recognizers: [
+        // RecognizerClass, [options], [recognizeWith, ...], [requireFailure, ...]
+        [Hammer.Rotate],
+        [Hammer.Pinch, { enable: false }, ['rotate']],
+        [Hammer.Swipe,{ direction: Hammer.DIRECTION_HORIZONTAL }],
+    ]
+});
+
+hammertime.on('tap', function(ev) {
+    alert(ev);
+});
+
+
+hammertime.add( new Hammer.Tap({ event: 'tap', taps: 1 }) );
